61012a8969f252b59d1840c44906996b3409e97e
Jeppe <jeppe.kronborg@pm.me>
2025-12-22 11:35:43 +0100

    added a usertypeenum, and also added it to the database

--- server/RiotProxy/Infrastructure/External/Domain/Entities/User.cs
+++ server/RiotProxy/Infrastructure/External/Domain/Entities/User.cs
@@ -1,8 +1,12 @@
+using RiotProxy.External.Domain.Enums;
+
 namespace RiotProxy.External.Domain.Entities
 {
     public class User : EntityBase
     {
         public int UserId { get; set; }
-        public string UserName { get; set; } = string.Empty;      
+        public string UserName { get; set; } = string.Empty;    
+
+        public UserTypeEnum UserType { get; set; } = UserTypeEnum.Solo;  
     }
 }
---
+++ server/RiotProxy/Infrastructure/External/Domain/Enums/UserTypeEnum.cs
@@ -0,0 +1,8 @@
+namespace RiotProxy.External.Domain.Enums;
+
+public enum UserTypeEnum
+{
+    Solo = 0,
+    Duo = 1,
+    Team = 2
+}
