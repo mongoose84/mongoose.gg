aed8a7f6582052d6beb66151876ae0ebf802f780
Jeppe <jeppe.kronborg@pm.me>
2026-01-22 12:10:53 +0100

    Removed the old references to the agileastronaut domain, and added the new mongoose.gg domain. And only supporting https for addes security

--- server/Program.cs
+++ server/Program.cs
@@ -137,10 +137,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:5173", // Vue dev server (default port)
                 "http://localhost:5174", // Vue dev server (alternate port)
                 "http://localhost:5175", // Vue dev server (alternate port)
-                "http://lol.agileastronaut.com",
-                "https://lol.agileastronaut.com",
-                "http://www.lol.agileastronaut.com",
-                "https://www.lol.agileastronaut.com"
+                "https://mongoose.gg",
+                "https://www.mongoose.gg"
                )
               .AllowAnyHeader()                      // allow all custom headers (Content-Type, Authorization, etc.)
               .AllowAnyMethod()                      // GET, POST, PUT, DELETE, OPTIONS…
