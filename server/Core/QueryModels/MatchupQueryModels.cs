2b3d69257c69be8c933adc2fcc73fe5b27416632
Jeppe <jeppe.kronborg@pm.me>
2026-02-10 19:45:09 +0100

    updated the code based code review

--- server/Core/QueryModels/MatchupQueryModels.cs
+++ server/Core/QueryModels/MatchupQueryModels.cs
@@ -2,7 +2,10 @@ namespace Mongoose.Api.Core.QueryModels;
 
 /// <summary>
 /// Core layer data models for champion matchup queries.
-/// These are returned by repositories and mapped to DTOs in endpoints.
+/// NOTE: These models are not currently in use. Repositories still return Application DTOs directly.
+/// These were created as part of a planned Clean Architecture refactoring (Phase 3) that was not completed.
+/// To use these, repositories would need to be updated to return these Core types,
+/// and endpoints would map them to Application DTOs.
 /// </summary>
 
 /// <summary>
