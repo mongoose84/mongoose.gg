8304909442f0d02e33f71982b6432d6ba4cdf904
Jeppe <jeppe.kronborg@pm.me>
2026-02-27 19:01:41 +0100

    Updated the naming from Team to Team Analytics

--- client/src/components/AppSidebar.vue
+++ client/src/components/AppSidebar.vue
@@ -85,12 +85,12 @@
       <router-link
         to="/app/team"
         class="nav-item flex items-center gap-md p-md mx-sm text-text-secondary no-underline rounded-md cursor-pointer whitespace-nowrap hover:bg-background-elevated hover:text-text"
-        :title="isCollapsed ? 'Team' : ''"
+        :title="isCollapsed ? 'Team Analytics' : ''"
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="nav-icon w-5 h-5 shrink-0">
           <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
         </svg>
-        <span v-if="!isCollapsed" class="nav-label text-sm font-medium tracking-tight">Team</span>
+        <span v-if="!isCollapsed" class="nav-label text-sm font-medium tracking-tight">Team Analytics</span>
       </router-link>
 
       <router-link
