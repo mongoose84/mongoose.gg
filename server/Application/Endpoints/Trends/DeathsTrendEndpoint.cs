c702cba2bacff32b060273ab2df87000bb5e25f1
Jeppe Kronborg <jeppe.kronborg@twoday.com>
2026-03-29 23:30:25 +0200

    feat: include tag line in game names for trend endpoints and solo stats page

--- server/Application/Endpoints/Trends/DeathsTrendEndpoint.cs
+++ server/Application/Endpoints/Trends/DeathsTrendEndpoint.cs
@@ -47,7 +47,7 @@ public sealed class DeathsTrendEndpoint : IEndpoint
                     return accountError;
 
                 var puuids = resolvedAccounts!.Select(a => a.Account.Puuid).ToList();
-                var puuidToGameName = resolvedAccounts!.ToDictionary(a => a.Account.Puuid, a => a.Account.GameName);
+                var puuidToGameName = resolvedAccounts!.ToDictionary(a => a.Account.Puuid, a => $"{a.Account.GameName}#{a.Account.TagLine}");
 
                 // Validate limit if provided
                 int? validatedLimit = null;
